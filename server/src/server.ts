0b245aae17c0ce3c12d3f4c2acbba13381a3a244
rbpolim <rbpolim@gmail.com>
2023-01-18 14:08:53 -0300

    feat: add repo remote gh

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+import { prisma } from "./lib/prisma";
+
+const app = Fastify();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("🚀 Server is running 🚀");
+  });
---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1 className="text-red-300">Hello World</h1>
+    </div>
+  )
+}
